9f1a211d6dd942d8da3c828607f45a4878ac5a45
Fitz50 <116603134+Fitz50@users.noreply.github.com>
2022-11-09 21:51:52 -0500

    Add files via upload

--- Mental Health/src/mentalHealth/userData.java
+++ Mental Health/src/mentalHealth/userData.java
@@ -2,22 +2,22 @@ package mentalHealth;
 
 public class userData {
 	
-	private String date;	//Variable Declaration
+	private int date;	//Variable Declaration
 	private String notes;
 	private int rating;
 	
-	public userData(int rating,String notes, String date) {	//Constructor for Data Entries
+	public userData(int rating,String notes, int date) {	//Constructor for Data Entries
 		super();
 		this.date = date;
 		this.notes = notes;
 		this.rating = rating;
 	}
 
-	public String getDate() { //Getters and Setters
+	public int getDate() { //Getters and Setters
 		return date;
 	}
 
-	public void setDate(String date) {
+	public void setDate(int date) {
 		this.date = date;
 	}
 
--- Mental Health/src/mentalHealth/userSaveData.java
+++ Mental Health/src/mentalHealth/userSaveData.java
@@ -7,10 +7,16 @@ import java.io.PrintWriter;
 
 public class userSaveData { //date,rating,notes
 	
-	userData [] userMonth = new userData[30];
-	
+	userData [] dataArray = new userData[365];
+
+	for (int i=0; i< dataArray.length; i++) {
+		String outputText = dataArray[i].getRating() + "|" + dataArray[i].getNotes() + "|" + dataArray[i].getDate();
+	}
+
+	saveToFile("dataList.txt", outputText, true);
+
 	public void saveUserData(userData segment) {
 		segment.getDate();
 	}
-	
+
 }
