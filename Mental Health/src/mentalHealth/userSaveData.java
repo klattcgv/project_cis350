60538337cc857b39201541aba72ef2d25a5b3f51
Fitz50 <116603134+Fitz50@users.noreply.github.com>
2022-11-09 22:27:08 -0500

    Add files via upload

--- Mental Health/src/mentalHealth/userSaveData.java
+++ Mental Health/src/mentalHealth/userSaveData.java
@@ -8,15 +8,26 @@ import java.io.PrintWriter;
 public class userSaveData { //date,rating,notes
 	
 	userData [] dataArray = new userData[365];
-
-	for (int i=0; i< dataArray.length; i++) {
-		String outputText = dataArray[i].getRating() + "|" + dataArray[i].getNotes() + "|" + dataArray[i].getDate();
-	}
-
-	saveToFile("dataList.txt", outputText, true);
+	private String outputText;
 
 	public void saveUserData(userData segment) {
 		segment.getDate();
+
+		for (int i=0; i< dataArray.length; i++) {
+			String outputText = dataArray[i].getRating() + "|" + dataArray[i].getNotes() + "|" + dataArray[i].getDate();
+		}
+
+		saveFile("dataList.txt", outputText, true);
+	}
+
+	public static void saveFile(String name, String text, boolean append) throws IOException{
+		File file1 = new File("save.txt");
+
+		FileWriter fw - new FileWriter(file1);
+
+		PrintWriter pw = new PrintWriter(fw);
+
+		pw.close();
 	}
 
 }
