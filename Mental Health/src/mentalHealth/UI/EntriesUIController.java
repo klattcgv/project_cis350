f424bb1df531904c9082f2b9a2aaea982d4e0e3a
Don Nguyen <donnguyen@Dons-MacBook-Pro.local>
2022-12-07 21:01:50 -0500

    new ui controller

--- Mental Health/src/mentalHealth/UI/EntriesUIController.java
+++ Mental Health/src/mentalHealth/UI/EntriesUIController.java
@@ -1,12 +1,27 @@
 package mentalHealth.UI;
 
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.time.LocalDate;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 import mentalHealth.Main;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
 
-public class EntriesUIController {
-    private Main main = new Main();
+public class EntriesUIController implements Initializable{
+    //private Main main = new Main();
+
+    @FXML
+    private DatePicker myDate;
+
+    @FXML
+    private ChoiceBox<String> myMood;
+
+    private String[] mood = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
 
     @FXML
     private void saveEntries() throws IOException {
@@ -43,4 +58,15 @@ public class EntriesUIController {
 
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        myMood.getItems().addAll(mood);
+        myMood.setOnAction(this::getMood);
+    }
+
+    int moodRate = 0;
+
+    private void getMood(javafx.event.ActionEvent actionEvent){
+        moodRate = Integer.parseInt(myMood.getValue());
+    }
 }
